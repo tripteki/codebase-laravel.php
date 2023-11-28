cc6d576defd4cc01e1ddc1120f54d93d1d0b85b7
hsbmaulana <hsbmaulana.iso@protonmail.com>
2023-11-28 11:25:43 +0700

    Initial Codebase : Swagger Documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,19 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="Application",
+ *      version="1.0"
+ * ),
+ * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      in="header",
+ *      type="http",
+ *      scheme="bearer",
+ *      bearerFormat="JWT"
+ * )
+ */
 class Controller extends BaseController
 {
     use Authorizable, AuthorizesRequests;
