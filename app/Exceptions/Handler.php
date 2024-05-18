0918acb7073b6f6f64d36b286fdade55928b6b27
Hasby Maulana <hsbmaulana.iso@protonmail.com>
2024-05-18 19:10:14 +0700

    Re-Convention

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -28,11 +33,76 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse|mixed
+     */
+    public function render($request, Throwable $thrower)
+    {
+        if ($thrower instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
+
+            $field = app($thrower->getModel())->getKeyName();
+            $id = $thrower->getIds();
+
+            $data = [
+
+                $field => [ __("validation.exists", [ "attribute" => Str::lower(Str::headline($field)), ]), ],
+            ];
+
+            return iresponse($data, Response::HTTP_UNPROCESSABLE_ENTITY, "Data not found.");
+        }
+
+        $response = $thrower;
+
+        return parent::render($request, $response);
+    }
+
+    /**
      * @return void
      */
     public function register()
     {
         $this->reportable(function (Throwable $thrower) {
+
+            //
+        });
+
+        //
+
+        $this->renderable(function (\Illuminate\Validation\ValidationException $exception) {
+
+            return iresponse($exception->errors(), Response::HTTP_UNPROCESSABLE_ENTITY, "Data not validated.");
+        });
+
+        //
+
+        $this->renderable(function (\Tymon\JWTAuth\Exceptions\TokenInvalidException $exception) {
+
+            return iresponse([], Response::HTTP_UNAUTHORIZED, "Token is invalid.");
+        });
+
+        $this->renderable(function (\Tymon\JWTAuth\Exceptions\TokenExpiredException $exception) {
+
+            return iresponse([], Response::HTTP_UNAUTHORIZED, "Token has expired.");
+        });
+
+        $this->renderable(function (\Tymon\JWTAuth\Exceptions\TokenBlacklistedException $exception) {
+
+            return iresponse([], Response::HTTP_UNAUTHORIZED, "Token has blacklisted.");
+        });
+
+        $this->renderable(function (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $exception) {
+
+            return iresponse([], Response::HTTP_UNAUTHORIZED, "User is not defined.");
+        });
+
+        $this->renderable(function (\Tymon\JWTAuth\Exceptions\InvalidClaimException $exception) {
+
+            return iresponse([], Response::HTTP_UNAUTHORIZED, "Claim is invalid.");
+        });
+
+        $this->renderable(function (\Tymon\JWTAuth\Exceptions\PayloadException $exception) {
+
+            return iresponse([], Response::HTTP_UNAUTHORIZED, "Payload is invalid.");
         });
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -22,4 +23,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use Authorizable, AuthorizesRequests;
+
+    /**
+     * @return array
+     */
+    protected function datas()
+    {
+        return $datas = array_merge(Route::getCurrentRoute()->parameters(), request()->all());
+    }
 }
